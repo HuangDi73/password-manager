d39813f4062cf7a2e16b900878af41d9eae064ba
HuangDi73 <dahaka0@mail.ru>
2025-02-20 16:04:16 +0300

    Добавлена функция получения информации от пользователя с консоли.

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+var variants = []string{
+	"1. Создать аккаунт",
+	"2. Найти аккаунт по url",
+	"3. Найти аккаунт по логину",
+	"4. Удалить аккаунт по url",
+	"5. Выход",
+	"Введите вариант",
+}
+
+func main() {
+	choices := promptData(variants...)
+	fmt.Println(choices)
+}
+
+func promptData(prompts ...string) string {
+	for i, p := range prompts {
+		if len(prompts)-1 == i {
+			fmt.Printf("%v: ", p)
+		} else {
+			fmt.Println(p)
+		}
+	}
+	var input string
+	fmt.Scanln(&input)
+	return input
 }
