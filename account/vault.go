76aa874ac37c55efa3af3ef5ae157edd56cae112
HuangDi73 <dahaka0@mail.ru>
2025-02-20 20:14:23 +0300

    Добавлена структура VaultWithDB, её конструктор, а также возможность сериализации и десериализации структуры Vault.

--- account/vault.go
+++ account/vault.go
@@ -1,6 +1,11 @@
 package account
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/fatih/color"
+)
 
 type DB interface {
 	Read() ([]byte, error)
@@ -11,4 +16,49 @@ type Vault struct {
 	Accounts  []Account `json:"accounts"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type VaultWithDB struct {
+	Vault
+	db DB
+}
+
+func NewVault(db DB) *VaultWithDB {
+	data, err := db.Read()
+	if err != nil {
+		color.Red("Не удалось прочитать файл")
+		return &VaultWithDB{
+			Vault: &Vault{
+				Accounts:  []Accounts{},
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+			db: DB,
+		}
+	}
+	var vault Vault
+	err := json.Unmarshal(data, &vault)
+	if err != nil {
+		color.Red("Не удалось разобрать json")
+		return &VaultWithDB{
+			Vault: &Vault{
+				Accounts:  []Accounts{},
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+			db: DB,
+		}
+	}
+	return &VaultWithDB{
+		Vault: vault,
+		db:    DB,
+	}
+}
+
+func (vault *Vault) toBytes() ([]byte, error) {
+	data, err := json.MarshalIndent(vault, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
